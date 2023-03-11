ea7bfc273e8d52f8c882b8892b17b36e8d6265cf
altonliew25 <awsmi@deakin.edu.au>
2023-03-11 16:58:41 +1100

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+var express = require("express");
+var app = express();
 
+//respond with "hello world!" when a GET request is made to the homepage
+app.get("/", function (req, res) {
+  //Get method facilitates you to send only limited amount of data because
+  res.send("hello world!");
+});
+
+//listen to a particular port
+app.listen(3000);
